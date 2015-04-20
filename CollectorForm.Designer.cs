6b979a2f1c353046ac8aebe2c7bdeabc3ad2d846
InJung Chung <mu29gl@gmail.com>
2015-04-20 23:28:53 +0900

    HTML 파싱
    파싱 후 다운로드 시작

---
+++ CollectorForm.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace VideoCollector
+{
+    partial class CollectorForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webPanel = new System.Windows.Forms.Panel();
+            this.mWebBrowser = new System.Windows.Forms.WebBrowser();
+            this.textUrl = new System.Windows.Forms.TextBox();
+            this.btnCollect = new System.Windows.Forms.Button();
+            this.textLog = new System.Windows.Forms.TextBox();
+            this.webPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webPanel
+            // 
+            this.webPanel.Controls.Add(this.mWebBrowser);
+            this.webPanel.Location = new System.Drawing.Point(12, 207);
+            this.webPanel.Name = "webPanel";
+            this.webPanel.Size = new System.Drawing.Size(349, 100);
+            this.webPanel.TabIndex = 1;
+            // 
+            // mWebBrowser
+            // 
+            this.mWebBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mWebBrowser.Location = new System.Drawing.Point(0, 0);
+            this.mWebBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.mWebBrowser.Name = "mWebBrowser";
+            this.mWebBrowser.Size = new System.Drawing.Size(349, 100);
+            this.mWebBrowser.TabIndex = 0;
+            this.mWebBrowser.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.mWebBrowser_DocumentCompleted);
+            // 
+            // textUrl
+            // 
+            this.textUrl.Location = new System.Drawing.Point(12, 12);
+            this.textUrl.Name = "textUrl";
+            this.textUrl.Size = new System.Drawing.Size(268, 21);
+            this.textUrl.TabIndex = 2;
+            // 
+            // btnCollect
+            // 
+            this.btnCollect.Location = new System.Drawing.Point(286, 10);
+            this.btnCollect.Name = "btnCollect";
+            this.btnCollect.Size = new System.Drawing.Size(75, 23);
+            this.btnCollect.TabIndex = 3;
+            this.btnCollect.Text = "수집하기";
+            this.btnCollect.UseVisualStyleBackColor = true;
+            this.btnCollect.Click += new System.EventHandler(this.btnCollect_Click);
+            // 
+            // textLog
+            // 
+            this.textLog.Location = new System.Drawing.Point(12, 39);
+            this.textLog.Multiline = true;
+            this.textLog.Name = "textLog";
+            this.textLog.Size = new System.Drawing.Size(349, 162);
+            this.textLog.TabIndex = 4;
+            // 
+            // CollectorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(373, 319);
+            this.Controls.Add(this.textLog);
+            this.Controls.Add(this.btnCollect);
+            this.Controls.Add(this.textUrl);
+            this.Controls.Add(this.webPanel);
+            this.Name = "CollectorForm";
+            this.Text = "VideoCollector";
+            this.webPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel webPanel;
+        private System.Windows.Forms.WebBrowser mWebBrowser;
+        private System.Windows.Forms.TextBox textUrl;
+        private System.Windows.Forms.Button btnCollect;
+        private System.Windows.Forms.TextBox textLog;
+
+    }
+}
+
---
+++ CollectorForm.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Collections;
+
+namespace VideoCollector
+{
+    public partial class CollectorForm : Form
+    {
+        Dictionary<String, DownFile> downloadFileList = new Dictionary<String, DownFile>();
+
+        public CollectorForm()
+        {
+            InitializeComponent();
+        }
+
+        private String GetString(char separator, String identifier, String text)
+        {
+            String[] candidate = text.Split(separator);
+            String value = "";
+
+            for (int i = 0; i < candidate.Length; i++)
+            {
+                if (candidate[i].Contains(identifier))
+                {
+                    value = candidate[i + 1];
+                    break;
+                }
+            }
+
+            return value;
+        }
+
+        private void StartSeek()
+        {
+            HtmlDocument doc = mWebBrowser.Document;
+            String fileName = GetString('>', "video_title", doc.Body.InnerHtml).Replace("</DIV", "");
+            String fileUrl = GetString('\'', "hq_video_file", doc.Body.InnerHtml);
+
+            if (!downloadFileList.ContainsKey(fileName))
+            {
+                DownFile downFile = new DownFile(fileName, fileUrl, this);
+                downloadFileList.Add(fileName, downFile);
+                downFile.startDownload();
+            }
+        }
+
+        public void Log(string text)
+        {
+            textLog.AppendText(text + "\r\n");
+        }
+
+        private void btnCollect_Click(object sender, EventArgs e)
+        {
+            if (textUrl.Text != "")
+            {
+                mWebBrowser.Navigate(textUrl.Text);
+            }
+        }
+
+        private void mWebBrowser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            StartSeek();
+        }
+    }
+}
